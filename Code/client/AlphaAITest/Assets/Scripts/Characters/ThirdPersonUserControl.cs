85de9753f82c52535dee68e27d328869070e4802
unmorally2022 <unmorally2022@gmail.com>
2022-12-14 17:19:57 +0800

    Update ThirdPersonUserControl.cs

--- Code/client/AlphaAITest/Assets/Scripts/Characters/ThirdPersonUserControl.cs
+++ Code/client/AlphaAITest/Assets/Scripts/Characters/ThirdPersonUserControl.cs
@@ -8,7 +8,7 @@ namespace AlphaAITest.Characters.ThirdPerson
     //[RequireComponent(typeof (ThirdPersonCharacter))]
     public class ThirdPersonUserControl : MonoBehaviour
     {
-        
+
         public ThirdPersonCharacterSP m_Character; // A reference to the ThirdPersonCharacter on the object
         private Transform m_Cam;                  // A reference to the main camera in the scenes transform
         private Vector3 m_CamForward;             // The current forward direction of the camera
@@ -77,51 +77,58 @@ namespace AlphaAITest.Characters.ThirdPerson
                 //}
             }
 
-            if (characterState == AppManager.CharacterState.ready && AppManager.gameplayState == AppManager.GameplayState.play)
-            {
-                //send movement to server
-                SendMovement();
 
-                if (IFChat.isFocused == true)
+            //send movement to server
+            SendMovement();
+
+            if (IFChat.isFocused == true)
+            {
+                ////if (CrossPlatformInputManager.GetButtonUp("return")) {                        
+
+                ////}
+                //if (Input.GetKeyUp(KeyCode.Return)) {
+                //    gamePlayManager.GUI_sendChat();
+                //    Debug.Log("IFChat focus");
+                //}
+            }
+            else
+            {
+                //Debug.Log("IFChat not focus");
+                // read inputs
+                float h = 0;
+                float v = 0;
+                if (characterState == AppManager.CharacterState.ready && AppManager.gameplayState == AppManager.GameplayState.play)
                 {
-                    ////if (CrossPlatformInputManager.GetButtonUp("return")) {                        
-                        
-                    ////}
-                    //if (Input.GetKeyUp(KeyCode.Return)) {
-                    //    gamePlayManager.GUI_sendChat();
-                    //    Debug.Log("IFChat focus");
-                    //}
+                    h = CrossPlatformInputManager.GetAxis("Horizontal");
+                    v = CrossPlatformInputManager.GetAxis("Vertical");
+                }
+
+                // calculate move direction to pass to character
+                if (m_Cam != null)
+                {
+                    // calculate camera relative direction to move:
+                    m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
+                    m_Move = v * m_CamForward + h * m_Cam.right;
                 }
                 else
                 {
-                    //Debug.Log("IFChat not focus");
-                    // read inputs
-                    float h = CrossPlatformInputManager.GetAxis("Horizontal");
-                    float v = CrossPlatformInputManager.GetAxis("Vertical");
+                    // we use world-relative directions in the case of no main camera
+                    m_Move = v * Vector3.forward + h * Vector3.right;
+                }
 
-                    // calculate move direction to pass to character
-                    if (m_Cam != null)
-                    {
-                        // calculate camera relative direction to move:
-                        m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-                        m_Move = v * m_CamForward + h * m_Cam.right;
-                    }
-                    else
-                    {
-                        // we use world-relative directions in the case of no main camera
-                        m_Move = v * Vector3.forward + h * Vector3.right;
-                    }
-
+                if (m_Character != null)
+                {
                     // pass all parameters to the character control script
                     m_Character.Move(m_Move, m_Jump);
                     m_Jump = false;
                 }
             }
+
         }
 
         private void SendMovement()
         {
-            if (PlayerIOId == PlayerIOManager.PlayerIOid)
+            if (PlayerIOId == PlayerIOManager.PlayerIOid && m_Character != null)
             {
                 PlayerIOManager.SendMsg("Move",
                     m_Character.transform.position.x,
@@ -153,7 +160,8 @@ namespace AlphaAITest.Characters.ThirdPerson
             return PlayerIOId;
         }
 
-        public string getPlayerIOName() {
+        public string getPlayerIOName()
+        {
             return PlayerIOName;
         }
     }
